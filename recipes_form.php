665427ce45e0342ca7b0427e492c6a01f763e90d
17056079d <49056365+17056079d@users.noreply.github.com>
2021-04-05 00:11:48 +0800

    Update version of store image and recipes form

--- recipes_form.php
+++ recipes_form.php
@@ -1,10 +1,64 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <html lang="en">
+<script>
+var stepcount =4;
+var Ingcount=4;
+function deleteIng(){
+	if(Ingcount>=3){
+		document.getElementById("ingredients").deleteRow(Ingcount-1);
+		Ingcount--;
+	}
+	else{
+		alert("There should be at least one ingredient");
+	}
+}
+function deleteStep(){
+	if(Ingcount>=3){
+		document.getElementById("step").deleteRow(stepcount-1);
+		stepcount--;
+	}
+	else{
+		alert("There should be at least one step");
+	}
+}
+function addIng(){
+	var IngName="ingredients"+Ingcount;
+	var Amount="amount"+Ingcount;
+	document.getElementById('ingredients').insertAdjacentHTML('beforeend', '<tr><th>'+Ingcount+'</th><th><textarea name="'+IngName+'" cols="40" rows="2"></textarea></th><th><textarea name="'+Amount+'" cols="10" ></textarea></th></tr>');
+	Ingcount ++;
+}
+function addStep(){
+	var stepName="step"+stepcount;
+	var step = document.getElementById('step').insertAdjacentHTML('beforeend', '<tr><th>'+stepcount+'</th><th><textarea name="'+stepName+'" cols="40" rows="2"></textarea></th></tr>');
+	stepcount ++;
+}
+function handin(){
+	function createCookie(name, value, days) {
+    var expires;
+      
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+      
+    document.cookie = escape(name) + "=" + 
+        escape(value) + expires + "; path=/";
+}
+createCookie("stepcount", stepcount, "10");
+createCookie("ingcount", Ingcount, "10");
+}
+</script>
+
 <head>
   <title>Recipes Uploads</title>
 </head>
 <body>
   <h1>Uploads</h1>
-    <form action="store_image.php" method="POST" enctype="multipart/form-data">
+    <form action="store_image.php" method="POST" enctype="multipart/form-data" id="upload">
       Recipes Name: <input type="text" name="rname" /><br />
       Category: <select name="category">
     <option value="drink">drink</option>
@@ -20,7 +74,50 @@
   </select><br />
       Introduction: <br /><textarea name="introduction" cols="40" rows="5"></textarea><br />
       image: <input type="file" name="myimage" id="myimage"/><br />
-      <input type="submit" value="Upload" />
+	  Ingredients:
+	  <table id = "ingredients">
+	  <tr>
+	  <th>Item</th>
+	  <th>Name</th>
+	  <th>Amount</th>
+	  </tr>
+	  <tr>
+	  <th>1</th>
+	  <th><textarea name="ingredients1" cols="40" rows="2"></textarea></th><th><textarea name="amount1" cols="10" ></textarea></th>
+	  </tr>
+	  <tr>
+	  <th>2</th>
+	  <th><textarea name="ingredients2" cols="40" rows="2"></textarea></th><th><textarea name="amount2" cols="10" ></textarea></th>
+	  </tr>
+	  <tr>
+	  <th>3</th>
+	  <th><textarea name="ingredients3" cols="40" rows="2"></textarea></th><th><textarea name="amount3" cols="10" ></textarea></th>
+	  </tr>
+	  </table>
+	  <input type="button" class="Ing" value="+" onclick="addIng()"/>
+	  <input type="button" class="Ing" value="-" onclick="deleteIng()"/><br>
+	  Step:
+	  <table id = "step">
+	  <tr>
+	  <th>Step</th>
+	  <th>instruction</th>
+	  </tr>
+	  <tr>
+	  <th>1</th>
+	  <th><textarea name="step1" cols="40" rows="2"></textarea></th>
+	  </tr>
+	  <tr>
+	  <th>2</th>
+	  <th><textarea name="step2" cols="40" rows="2"></textarea></th>
+	  </tr>
+	  <tr>
+	  <th>3</th>
+	  <th><textarea name="step3" cols="40" rows="2"></textarea></th>
+	  </tr>
+	  </table>
+	  <input type="button" value="+" class="step" onclick="addStep()"/>
+	  <input type="button" value="-" class="step" onclick="deleteStep()"/><br>
+      <input type="submit" value="Upload" onclick="handin()"/>
     </form>
 </body>
 </html>
--- store_image.php
+++ store_image.php
@@ -1,6 +1,5 @@
 <?php
 include("config.php");
-
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD, "cooking_recipes");
 $RNameError = $IntroductionError = "";
 if (empty($_POST["rname"])) {
@@ -25,13 +24,39 @@ if (empty($_POST["rname"])) {
 else{
 	$imagepath="/xampp/htdocs/";
 }
+
 if ($RNameError == "" && $IntroductionError == "") {
 
 $sql="INSERT INTO recipes (RName,Introduction,Imagepath,Category,Imagename,Cuisine)VALUES('$RName','$Introduction','$imagepath','$Category','$imagename','$Cuisine')";
 
 if (mysqli_query($db, $sql) or die(mysqli_error($db))) {
-            $message = "Recipe upload successfully";
-        }
+	$message = "Recipe upload successfully";
+	$sql="SELECT RID FROM recipes WHERE RName='$RName' AND Introduction='$Introduction' AND Category='$Category'";
+	$result = mysqli_query($db, $sql);
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			$id= $row["RID"];
+		}
+	}
+	for ($i=1;$i<=$_COOKIE["ingcount"]-1;$i++){
+		$a = "ingredients";
+		$b = "amount";
+		$ing=$_POST[$a.$i];
+		$amo=$_POST[$b.$i];
+		$sql="INSERT INTO ingredients (RID,InName,Amount)VALUES('$id','$ing','$amo')";
+		mysqli_query($db, $sql);
+	}
+	for ($j=1;$j<=$_COOKIE["stepcount"]-1;$j++){
+		$c = "step";
+		$step=$_POST[$c.$j];
+		//echo($step);
+		$sql="INSERT INTO step (RID,Method)VALUES('$id','$step')";
+		mysqli_query($db, $sql);
+	}
+	mysqli_close($db);
+			
+}
 else {
             $message = "Fail to upload the recipe";
         }
@@ -41,9 +66,12 @@ else {
 else {
         if ($RNameError != "") $message = $RNameError;
         else if ($IntroductionError != "") $message = $IntroductionError;
-    }
-	mysqli_close($db);
+}
 
     // the response to android mobile
+	//echo ($_COOKIE["stepcount"]);
+	//$a = "ingredients";
+	//$b="1";
+	//echo ($_POST[$a.$b]);
     echo($message);
 ?>
